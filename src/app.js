8a7b52c2bfd36f3dc46b7037c4bcdc659ae2cf78
Gawah Chan <gawahchan@gmail.com>
2019-07-05 18:38:02 +0100

    Using arrays in JSX for form inputs

--- src/app.js
+++ src/app.js
@@ -30,19 +30,22 @@ const render = () => {
         <div>
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options && app.options.length > 0 ? 'Here are your options': 'No options'}</p>
+            <p>{app.options && app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
             <p>{app.options.length}</p>
             <button onClick={onRemoveAll}>Remove All</button>
             <ol>
-                <li>item one</li>
-                <li>item two</li>
+                {
+                    app.options.map(option => {
+                        return <li key={option}>{option}</li>
+                    })
+                }
             </ol>
-            <form onSubmit={onFormSubmit}> 
-                <input type="text" name="option"/>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
                 <button>Add option</button>
             </form>
         </div>
-        );
+    );
     ReactDOM.render(template, appRoot);
 }
 render();
