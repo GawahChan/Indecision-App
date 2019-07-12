392435400f87df061a1322b6fcd78000c620bbc4
Gawah Chan <gawahchan@gmail.com>
2019-07-12 13:53:49 +0100

    Experimenting with subClasses ES6 JS

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -10,16 +10,52 @@ class Person {
     getGreeting() {
         return `Hi, I am ${this.name}!`;
     }
-
-getDescription() {
-    return `${this.name} is ${this.age} year(s) old`
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old.`
+    }
 }
+
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age)
+        this.major = major 
+    }
+    hasMajor() {
+        return !!this.major;       //Common way of checking if something is true or false
+    }
+    //Overwriting getDescription() Method
+    getDescription() {
+        let description = super.getDescription();
+
+        if(this.hasMajor()) {
+            description += ` Their major is ${this.major}`
+        }
+        return description;
+    }
 }
 
-//Creates instances of the construct, Allows you to reuse Person() as many times as you want
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age)
+        this.homeLocation = homeLocation;
+    }
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+            if(this.hasHomeLocation()) {
+                greeting += ` I'm vising from ${this.homeLocation}.`
+            }
+        return greeting;
+    }
+}
 
-const me = new Person('Gawah Chan', 22);
-console.log(me.getDescription());
+//Creates instances of the construct, Allows you to reuse them as many times as you want
 
-const other = new Person();
-console.log(other.getDescription);
+//experiment by switching new Student with new Person
+const me = new Traveler('Gawah Chan', 23, 'London');
+console.log(me.getGreeting());
+
+const other = new Traveler(undefined, undefined, 'Nowhere');
+console.log(other.getGreeting());
